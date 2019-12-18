faa8ccf309a7bc9bd40f77bcd91056c24a9b04c5
gaurab <gaurab@DESKTOP-J3QMBJF>
2019-12-17 21:12:19 -0800

    logout swal gone

--- WealthClock_25_11_2019_NEW/Controllers/AfterLogInController/DocumentsController.cs
+++ WealthClock_25_11_2019_NEW/Controllers/AfterLogInController/DocumentsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,7 @@ namespace WealthClock_25_11_2019_NEW.Controllers.AfterLogInController
 {
     public class DocumentsController : Controller
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
         // GET: Documents
         public ActionResult Index()
         {
@@ -17,11 +20,18 @@ namespace WealthClock_25_11_2019_NEW.Controllers.AfterLogInController
         {
             if (Request.IsAuthenticated)
             {
+                string ClientCode = User.Identity.Name.Split('|')[0];
+                string User_Email = User.Identity.Name.Split('|')[1];
+                SqlCommand com = new SqlCommand("select count(*) from UserDocs where User_email=@Email", conn);
+                com.Parameters.AddWithValue("@Email", User_Email);
+                conn.Open();
+                ViewBag.documentCount = com.ExecuteScalar();
+                conn.Close();
                 return View();
             }
             else
             {
-                return RedirectToAction("LoginPage", "Login");
+                return RedirectToAction("index", "home");
             }
         }
     }
