197d48cc26cfb2279227e294db8adc3361b58f90
gaurab <gaurab@DESKTOP-J3QMBJF>
2019-12-18 05:04:35 -0800

    login & registration...Auto search

---
+++ WealthClock_25_11_2019_NEW/Models/DocDetails .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WealthClock_25_11_2019_NEW.Models
+{
+    public class DocDetails
+    {
+        public string Docs_ID { get; set; }
+        public string Document_Type { get; set; }
+        public string Document_Title { get; set; }
+        public string Document_Path { get; set; }
+    }
+}
